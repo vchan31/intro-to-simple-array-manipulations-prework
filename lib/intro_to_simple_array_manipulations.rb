aecd6bb210c283ee68395ecf9d54d0df88213042
sensible-proc-7557 <vchan313@gmail.com>
2018-11-04 17:01:17 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_unshift (array1,string1)
+  array1.unshift(string1)
+end
+
+def using_push (array1,string1)
+  array1.push(string1)
+end
+
+def using_pop(array1)
+  array1.pop()
+end
+
+def pop_with_args (array1)
+  array1.pop(2)
+end
+
+def using_shift (array1)
+  array1.shift()
+end
+
+def shift_with_args(array1)
+  array1.shift(2)
+end
+
+def using_concat (array1,array2)
+  array1.concat array2
+end
+
+def using_insert (array1,element1)
+  array1.insert(4, element1)
+end
+
+def using_uniq(array1)
+  array1.uniq
+end
